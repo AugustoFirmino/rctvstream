b1cadd59aea2edfa997d036f88fc6c1ea6485913
Augusto Firmino <gugufirmino1l@gmail.com>
2026-07-12 14:33:09 +0100

    Configurar socket publico

--- src/components/socket.js
+++ src/components/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 
 const socket = io(
-  "https://seen-necklace-binary-assumption.trycloudflare.com/",
+  "https://https://rctvstream.onrender.com/",
   {
     transports:["websocket"]
   }
